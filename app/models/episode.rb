9504c18b25a4af1ba872bf10d5d5e4f2e0417ba1
Albert Bellonch Llargués <albert@itnig.net>
2012-11-04 21:21:34 +0100

    Add some specs on episodes

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,3 +1,15 @@
 class Episode < ActiveRecord::Base
   attr_accessible :end_time, :number, :season, :start_time, :title, :tv_series
+
+  before_validation :set_identifier
+
+  validates_presence_of :tv_series, :title, :season, :number, :start_time, :end_time, :identifier
+  validates_uniqueness_of :identifier
+
+  private
+
+  def set_identifier
+    return unless tv_series.present? && season.present? && number.present?
+    self.identifier = "#{tv_series} #{season}x#{number.to_s.rjust(2, '0')}"
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121104191135) do
+ActiveRecord::Schema.define(:version => 20121104200600) do
 
   create_table "episodes", :force => true do |t|
     t.string   "tv_series"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(:version => 20121104191135) do
     t.datetime "end_time"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "identifier"
   end
 
 end
