f2d99af9b01a1b663999ba4190cfc16a7dca2ab8
Albert Bellonch Llargués <albert@itnig.net>
2012-11-05 02:59:56 +0100

    Implement the GetTorrentForEpisodeWorker

--- app/workers/get_torrent_for_episode_worker.rb
+++ app/workers/get_torrent_for_episode_worker.rb
@@ -2,5 +2,12 @@ class GetTorrentForEpisodeWorker
   include Sidekiq::Worker
 
   def perform(episode_id)
+    episode = episode.find(episode_id)
+
+    if torrent_url = WalterBishop::Reese.get_torrent(episode)
+      # hand it to Tyrion
+    else
+      GetTorrentForEpisodeWorker.perform_in(2.hours, episode_id)
+    end
   end
 end
