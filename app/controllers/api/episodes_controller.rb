80a1ce9ec43da13d70b91f492b45028cf41cce55
Albert Bellonch Llargués <albert@bellonch.com>
2015-11-03 22:23:59 +0100

    Remove N+1 query in Api::EpisodesController

--- app/controllers/api/episodes_controller.rb
+++ app/controllers/api/episodes_controller.rb
@@ -1,7 +1,6 @@
 class Api::EpisodesController < API::BaseController
   def index
-    @episodes = Episode.all
-    @episodes = @episodes.oldest_first
+    @episodes = Episode.all.includes(:show).oldest_first
 
     render json: @episodes
   end
